4a7f5f007cb7572dbf89804f0e503821ac0078c0
GoldenArt2020 <goldenartandmakeovers12@gmail.com>
2026-08-01 10:31:06 +0100

    Remove all Amplify dependencies and backend folder, clean lockfile

--- src/lib/dataClient.ts
+++
@@ -1,5 +0,0 @@
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "../../amplify/data/resource";
-import "../../lib/amplify-config";
-
-export const client = generateClient<Schema>();
