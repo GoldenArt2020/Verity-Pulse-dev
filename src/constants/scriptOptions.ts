1f1891f2e7fdc6d89e4c87d56c4db38cc0909b96
GoldenArt2020 <goldenartandmakeovers12@gmail.com>
2026-08-08 06:25:21 +0100

    Add scriptOptions constants file

---
+++ src/constants/scriptOptions.ts
@@ -0,0 +1,6 @@
+export const SCRIPT_WORD_COUNT_OPTIONS = [5000, 10000, 15000] as const;
+export type ScriptWordCount = (typeof SCRIPT_WORD_COUNT_OPTIONS)[number];
+
+export function isValidScriptWordCount(value: unknown): value is ScriptWordCount {
+  return typeof value === "number" && (SCRIPT_WORD_COUNT_OPTIONS as readonly number[]).includes(value);
+}
